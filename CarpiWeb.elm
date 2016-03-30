d2cb4f6a30a9cd78b0f61eeb95374ba643a6eca8
Alexander Areshnikov <alex.areshnikov@plansource.com>
2016-03-29 20:10:53 -0600

    Implement Signals

--- CarpiWeb.elm
+++ CarpiWeb.elm
@@ -20,6 +20,7 @@ type alias Model =
   { power: Float,
     brakePower: Float,
     powerLevel: Float,
+    reversedPower: Bool,
     turnDirection: Int
   }
 
@@ -30,29 +31,88 @@ initialCar =
     power = 3,
     brakePower = 3,
     powerLevel = 0,
+    reversedPower = False,
     turnDirection = 0
   }
 
 -- UPDATE
 
-update : { x : Int, y : Int } -> Model -> Model
-update axis car =
-  let
-    directionalPower =
-      if axis.y > 0 || ( axis.y == 0 && car.powerLevel < 0 ) then car.power else -1*car.power
-    appliedBrakePower =
-      if axis.y == -1 && car.powerLevel > 0 || axis.y == 1 && car.powerLevel < 0 then car.brakePower * directionalPower else directionalPower
-    appliedPowerLevel =
-      if axis.y == 0 && car.powerLevel == 0 then car.powerLevel else car.powerLevel + appliedBrakePower
-    stabelizedZeroPowerLevel =
-      if abs (car.powerLevel + appliedPowerLevel) < car.power then 0 else appliedPowerLevel
-    getPowerLevel =
-      clamp -100 100 stabelizedZeroPowerLevel
-  in
-  {car |
-    turnDirection = axis.x,
-    powerLevel = getPowerLevel}
+type Action =
+  NoOp |
+  Left |
+  Right |
+  Accelerate |
+  AccelerateLeft |
+  AccelerateRight |
+  Reverse |
+  ReverseLeft |
+  ReverseRight
 
+
+update : Action -> Model -> Model
+update action car =
+  car
+    |> applyTorque action
+    --|> applyDirection action
+
+
+applyTorque : Action -> Model -> Model
+applyTorque action car =
+  let
+    decreasePowerLevel brakeApplied =
+      let
+        currenCarPower =
+          if brakeApplied then car.brakePower * car.power else car.power
+      in
+        (car.powerLevel - currenCarPower) |> normalizedPowerLevel currenCarPower
+
+    increasePowerLevel =
+      (car.powerLevel + car.power) |> normalizedPowerLevel car.power
+
+    normalizedPowerLevel currenCarPower newPowerLevel =
+      let
+        powerLevel =
+          if abs (car.powerLevel + newPowerLevel) < currenCarPower then 0 else newPowerLevel
+      in
+        clamp 0 100 powerLevel
+
+    newReversedPower newPowerLevel =
+      if newPowerLevel == 0 then False else car.reversedPower
+  in
+    if action == NoOp then
+      let
+        newPowerLevel =
+          if car.powerLevel == 0 then 0 else decreasePowerLevel False
+      in
+        { car |
+          powerLevel = newPowerLevel,
+          reversedPower = newReversedPower newPowerLevel }
+    else if List.any (\v -> action == v) [Accelerate, AccelerateLeft, AccelerateRight] then
+      let
+        newPowerLevel =
+          if car.reversedPower then
+            decreasePowerLevel True
+          else
+            increasePowerLevel
+      in
+        { car |
+          powerLevel = newPowerLevel,
+          reversedPower = newReversedPower newPowerLevel }
+    else if List.any (\v -> action == v) [Reverse, ReverseLeft, ReverseRight] then
+      let
+        newPowerLevel =
+          if car.reversedPower then
+            increasePowerLevel
+          else
+            decreasePowerLevel True
+        revReversedPower =
+          if car.powerLevel == 0 then True else car.reversedPower
+      in
+        { car |
+          powerLevel = newPowerLevel,
+          reversedPower = revReversedPower }
+      else
+        car
 -- VIEW
 
 view : (Int, Int) -> Model -> Element
@@ -71,16 +131,31 @@ drawCanvas w h =
   rect w h
     |> filled gray
 
-getAxis : Signal { x : Int, y : Int }
-getAxis =
+-- SIGNALS
+
+input : Signal Action
+input =
   let
     delta = Time.fps 30
+    toAction n =
+      case (n.x, n.y) of
+        (1,0) -> Right
+        (-1,0) -> Left
+        (0,1) -> Accelerate
+        (1,1) -> AccelerateRight
+        (-1,1) -> AccelerateLeft
+        (0,-1) -> Reverse
+        (1,-1) -> ReverseRight
+        (-1,-1) -> ReverseLeft
+        _ -> NoOp
+
+    actions = Signal.map toAction(Keyboard.arrows)
   in
-    Signal.sampleOn delta Keyboard.arrows
+    Signal.sampleOn delta actions
 
 model : Signal Model
 model =
-  Signal.foldp update initialCar getAxis
+  Signal.foldp update initialCar input
 
 
 main : Signal Element
